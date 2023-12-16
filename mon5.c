9e986750f5b7a698a4d3a6b4ab6f7bd6f8eecd91
yusrA312 <yusra2yahya@gmail.com>
2023-12-16 04:25:20 -0800

    done

--- mon5.c
+++ mon5.c
@@ -49,9 +49,9 @@ int pl(char *buf, int mon, int format)
 		return (format);
 	val = strtok(NULL, delimt);
 
-	if (_strcmp(code, "stack") == 0)
+	if (strcmp(code, "stack") == 0)
 		return (0);
-	if (_strcmp(code, "queue") == 0)
+	if (strcmp(code, "queue") == 0)
 		return (1);
 
 	f_func(code, val, mon, format);
