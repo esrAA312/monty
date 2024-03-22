3858de9e4472cf3ceddae2ba7507d1ad122a311a
esrAA312 <esraayahya312@gmail.com>
2024-03-22 04:23:59 -0700

    done

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/pup
+#install
+package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
+}
+package { 'python3-pip':
+    ensure   => installed
+}
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3'
+}
